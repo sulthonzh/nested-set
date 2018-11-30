116ad43a79ddebb177a40fb514e2e5ee8fcd2fe1
Sulthon Zainul Habib <sulthon.poetra.ahmad@gmail.com>
2018-11-30 17:42:40 +0700

    update int to int32

--- query.go
+++ query.go
@@ -4,7 +4,7 @@ type (
 	// Query ::
 	Query struct {
 		Get      string `json:"get"`       // all|child|parent
-		NodeType int    `json:"node_type"` //
+		NodeType int32  `json:"node_type"` //
 		NodeName string `json:"node_name"` //
 	}
 )
